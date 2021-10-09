5a389d23c72ab26311ba9c90283db4858759da55
asthabansal11 <67437836+asthabansal11@users.noreply.github.com>
2021-10-09 22:57:05 +0530

    Add files via upload

---
+++ TwoDArray.java
@@ -0,0 +1,9 @@
+public class TwoDArray {
+    public static void main(String args[]) {
+        // int a[] [] = new int[5] [3];
+        // System.out.println(a[2] [1]);
+
+        int b[][] = { { 3, 2, 4, 6, 7 }, { 2, 4, 5 }, { 5, 5, 4, 2, 7, 9 } };
+        System.out.println(b[2][4]);
+    }
+}
